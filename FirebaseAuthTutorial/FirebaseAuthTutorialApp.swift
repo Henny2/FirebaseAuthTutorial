1a311faff835b14e8e83c4b099c1624663005632
Henny2 <henrieke.baunack@web.de>
2024-10-13 10:47:41 -1000

    Initial Commit

---
+++ FirebaseAuthTutorial/FirebaseAuthTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthTutorialApp.swift
+//  FirebaseAuthTutorial
+//
+//  Created by Henrieke Baunack on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
