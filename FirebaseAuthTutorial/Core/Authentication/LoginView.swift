f8ee9e41413cd717a7b6ccb9645a46e1e86469e2
Henny2 <henrieke.baunack@web.de>
2024-10-13 11:45:08 -1000

    building the login view

--- FirebaseAuthTutorial/Core/Authentication/LoginView.swift
+++ FirebaseAuthTutorial/Core/Authentication/LoginView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
         NavigationStack{
             VStack{
@@ -19,13 +21,50 @@ struct LoginView: View {
                     .padding(.vertical, 32)
                 
                 // form fields
-                
+                VStack(spacing: 24){
+                    InputView(text: $email,
+                              title: "Email Address",
+                              placeholder: "name@example.com")
+                    .autocapitalization(.none) // don't want it to automatically capitalize
+                    
+                    InputView(text: $password,
+                              title: "Password",
+                              placeholder: "Enter your password",
+                              isSecuredFiled: true)
+                }
+                .padding(.horizontal)
+                .padding(.top, 12) // more space between logo and form
                 
                 // sign in button
                 
+                Button {
+                    print("Logging in")
+                } label: {
+                    HStack{
+                        Text("SIGN IN")
+                            .fontWeight(.semibold)
+                        Image(systemName: "arrow.right")
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: UIScreen.main.bounds.width-32, height: 48)
+                }
+                .background(Color(.systemBlue))
+                .cornerRadius(10)
+                .padding(.top, 24)
+                
                 Spacer()
                 
-                // sign up button
+                NavigationLink {
+                    Color.blue
+                } label: {
+                    HStack(spacing: 3){
+                        Text("Don't have an account?")
+                        Text("Sign up")
+                            .fontWeight(.bold)
+                    }
+                    .font(.system(size: 14))
+                }
+
             }
         }
     }
