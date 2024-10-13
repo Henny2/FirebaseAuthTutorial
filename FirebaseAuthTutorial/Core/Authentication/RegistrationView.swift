1642fbf34edd9652ffea56e2dc6340d6322dfdba
Henny2 <henrieke.baunack@web.de>
2024-10-13 11:53:51 -1000

    set up registrationview

---
+++ FirebaseAuthTutorial/Core/Authentication/RegistrationView.swift
@@ -0,0 +1,82 @@
+//
+//  RegistrationView.swift
+//  FirebaseAuthTutorial
+//
+//  Created by Henrieke Baunack on 10/13/24.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    
+    @State private var email = ""
+    @State private var fullName = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack{
+            Image("surfer")
+                .resizable()
+                .scaledToFill()
+                .frame(width:100, height:120)
+                .padding(.vertical, 32)
+            
+            VStack(spacing: 24){
+                InputView(text: $email,
+                          title: "Email Address",
+                          placeholder: "name@example.com")
+                .autocapitalization(.none) // don't want it to automatically capitalize
+                
+                InputView(text: $fullName,
+                          title: "Full Name",
+                          placeholder: "Enter your name")
+                
+                InputView(text: $password,
+                          title: "Password",
+                          placeholder: "Enter your password",
+                          isSecuredFiled: true)
+                
+                InputView(text: $confirmPassword,
+                          title: "Confirm Password",
+                          placeholder: "Confirm your password",
+                          isSecuredFiled: true)
+            }
+            .padding(.horizontal)
+            .padding(.top, 12)
+            
+            Button {
+                print("Signing user up")
+            } label: {
+                HStack{
+                    Text("SIGN UP")
+                        .fontWeight(.semibold)
+                    Image(systemName: "arrow.right")
+                }
+                .foregroundColor(.white)
+                .frame(width: UIScreen.main.bounds.width-32, height: 48)
+            }
+            .background(Color(.systemBlue))
+            .cornerRadius(10)
+            .padding(.top, 24)
+            
+            Spacer()
+            
+            Button {
+                dismiss()
+            } label: {
+                HStack(spacing: 3){
+                    Text("Already have an account?")
+                    Text("Sign in")
+                        .fontWeight(.bold)
+                }
+                .font(.system(size: 14))
+            }
+        }
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
