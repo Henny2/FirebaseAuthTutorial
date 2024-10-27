0eb3f07d0c22c0e42b12aa1883d9b01b00a9b140
Henny2 <henrieke.baunack@web.de>
2024-10-26 16:27:12 -1000

    finish profile view

--- FirebaseAuthTutorial/Core/Profile/ProfileView.swift
+++ FirebaseAuthTutorial/Core/Profile/ProfileView.swift
@@ -33,12 +33,31 @@ struct ProfileView: View {
 
             }
             Section("General"){
-                
-                
+                HStack {
+                    SettingsRowView(imageName: "gear", title: "Version", tintColor: Color(.systemGray))
+                    Spacer()
+                    Text("1.0.0")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
             }
-            
             Section("Account"){
-                
+                Button {
+                    print("logging out")
+                } label: {
+                    SettingsRowView(imageName: "arrow.left.circle.fill",
+                                    title: "Sign Out",
+                                    tintColor: .red)
+                }
+                Button {
+                    print("delete account")
+                } label: {
+                    SettingsRowView(imageName: "xmark.circle.fill",
+                                    title: "Delete Account",
+                                    tintColor: .red)
+                }
+
+
             }
         }
     }
