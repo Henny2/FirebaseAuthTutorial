efa3e1ba89e2e9347f17659908ad5074c22cd513
Henny2 <henrieke.baunack@web.de>
2024-11-09 20:17:09 -1000

    adding user creation method using firebase

--- FirebaseAuthTutorial/Core/Authentication/ViewModel/AuthViewModel.swift
+++ FirebaseAuthTutorial/Core/Authentication/ViewModel/AuthViewModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 import FirebaseAuth
 import Firebase
+import FirebaseFirestore
 
 // responsible for having all functionlity with authenticating the user, i.e. making the API calls
 // sending info/updates to the Views
@@ -26,9 +27,19 @@ class AuthViewModel: ObservableObject {
     func signIn(withEmail email: String, password: String) async throws {
         print("sign in ...")
     }
+    
     func createUser(withEmail email: String, password: String, fullName: String) async throws{
-        print("Create user...")
+        do {
+            let result = try await Auth.auth().createUser(withEmail: email, password: password)
+            self.userSession = result.user
+            let user = User(id: result.user.uid, fullName: fullName, email: email)
+            let encodedUser = try Firestore.Encoder().encode(user)
+            try await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
+        } catch {
+            print("DEBUG: Failed to create user with error \(error.localizedDescription)")
+        }
     }
+    
     func signOut(){
         print("sign out ...")
     }
